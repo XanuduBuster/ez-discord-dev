43df47c212c4061db5c00bdae098c153fe8be1a2
Xanudu <133562760+XanuduBuster@users.noreply.github.com>
2023-10-29 13:13:25 +0530

    create main bot

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from discord_slash import SlashCommand
+from token import TOKEN
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+slash = SlashCommand(bot, sync_commands=True)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+
+@slash.slash(name="test", description="test bot command.")
+async def test(ctx):
+    await ctx.send("wokring")
+
+@slash.slash(name="get_badge", description="get Developer Badge")
+async def new_command(ctx):
+    await ctx.send("you must run the test and this command multiple times, after this wait for 24 - 48 hours and check if you can claim the dev badge, if this didnt work, DM `xanudubuster`. thanks.")
+
+bot.run(TOKEN)
